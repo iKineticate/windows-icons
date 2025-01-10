cada598e9ef9e4caa0e7134da04f2ae0078eedd4
iKineticate <115683118+iKineticate@users.noreply.github.com>
2025-01-10 18:08:10 +0800

    Refactor: Change all function returns to Result<T, Box<dyn Error>> and other improvements
    - Refactored all public functions to return Result<T, Box<dyn Error>> for better error handling.
    - Improved error messages and handling throughout the codebase.
    - Added existence checks for manifest_path and icon_full_path in fn get_icon_file_path.
    - Simplified image and base64 conversion functions:
      - Combined read_image_to_base64 and get_icon_from_base64 into icon_to_image and icon_to_base64.
      - Removed unnecessary steps in get_uwp_icon to avoid redundant conversions.
    - Updated UWP icon path detection to more accurately locate UWP applications and exclude WSA applications:
    - Refactored and optimized various functions for better readability and maintainability.

--- examples/main.rs
+++ examples/main.rs
@@ -6,30 +6,30 @@ use windows_icons::{
 fn main() {
     let _ = std::fs::create_dir("output");
 
-    let icon = get_icon_by_path("C:\\Windows\\System32\\notepad.exe");
+    let icon = get_icon_by_path("C:\\Windows\\System32\\notepad.exe").unwrap();
     icon.save("output/notepad.png").unwrap();
 
-    let icon = get_icon_by_path("C:\\Windows\\System32\\calc.exe");
+    let icon = get_icon_by_path("C:\\Windows\\System32\\calc.exe").unwrap();
     icon.save("output/calc.png").unwrap();
 
-    let icon = get_icon_by_path("C:\\Windows\\System32\\cmd.exe");
+    let icon = get_icon_by_path("C:\\Windows\\System32\\cmd.exe").unwrap();
     icon.save("output/cmd.png").unwrap();
 
-    let base64 = get_icon_base64_by_path("C:\\Windows\\System32\\notepad.exe");
+    let base64 = get_icon_base64_by_path("C:\\Windows\\System32\\notepad.exe").unwrap();
     println!("Notepad: {}", base64);
 
-    let base64 = get_icon_base64_by_path("C:\\Windows\\System32\\calc.exe");
+    let base64 = get_icon_base64_by_path("C:\\Windows\\System32\\calc.exe").unwrap();
     println!("Calc: {}", base64);
 
-    let base64 = get_icon_base64_by_path("C:\\Windows\\System32\\cmd.exe");
+    let base64 = get_icon_base64_by_path("C:\\Windows\\System32\\cmd.exe").unwrap();
     println!("Cmd: {}", base64);
 
     // Substitute the process id to test
     let process_id = 0000;
 
-    let base64 = get_icon_base64_by_process_id(process_id);
+    let base64 = get_icon_base64_by_process_id(process_id).unwrap();
     println!("Process {}: {}", process_id, base64);
 
-    let icon = get_icon_by_process_id(process_id);
+    let icon = get_icon_by_process_id(process_id).unwrap();
     icon.save("output/process.png").unwrap();
 }
--- src/uwp_apps.rs
+++ src/uwp_apps.rs
@@ -1,38 +1,97 @@
-use std::{error::Error, fs, path::Path};
+use std::{
+    error::Error,
+    fs,
+    io::{self, ErrorKind},
+    path::Path,
+};
 
 use image::RgbaImage;
 
-use crate::utils::image_utils::{get_icon_from_base64, read_image_to_base64};
+use crate::utils::image_utils::{icon_to_base64, icon_to_image};
 
 pub fn get_uwp_icon(process_path: &str) -> Result<RgbaImage, Box<dyn Error>> {
     let icon_path = get_icon_file_path(process_path)?;
-    println!("Icon path: {}", icon_path);
-    let base64 = read_image_to_base64(&icon_path)?;
-    let icon = get_icon_from_base64(&base64)?;
-    Ok(icon)
+    let rgba_image = icon_to_image(&icon_path).map_err(|e| {
+        io::Error::new(
+            io::ErrorKind::Other,
+            format!("Failed to get icon image for path: '{process_path}'\n{e}"),
+        )
+    })?;
+    Ok(rgba_image)
 }
 
 pub fn get_uwp_icon_base64(process_path: &str) -> Result<String, Box<dyn Error>> {
     let icon_path = get_icon_file_path(process_path)?;
-    let base64 = read_image_to_base64(&icon_path)?;
+    let base64 = icon_to_base64(&icon_path).map_err(|e| {
+        io::Error::new(
+            io::ErrorKind::Other,
+            format!("Failed to get icon base64 for path: '{process_path}'\n{e}"),
+        )
+    })?;
     Ok(base64)
 }
 
 fn get_icon_file_path(app_path: &str) -> Result<String, Box<dyn Error>> {
-    let package_folder = Path::new(app_path).parent().unwrap();
+    if !Path::new(app_path).exists() {
+        return Err(Box::new(io::Error::new(
+            io::ErrorKind::NotFound,
+            format!("app path does not exist: {app_path}"),
+        )));
+    }
 
+    let package_folder = Path::new(app_path).parent().ok_or_else(|| {
+        io::Error::new(
+            ErrorKind::NotFound,
+            format!("failed to get parent directory: '{app_path}'"),
+        )
+    })?;
+
+    // let icon_path_vec = ["assets/logo.png", "assets/logo.ico", "assets/icon.png", "assets/DesktopShortcut.ico", ...]
     let desktop_icon_path = package_folder.join("assets").join("DesktopShortcut.ico");
-
     if desktop_icon_path.exists() {
-        return Ok(desktop_icon_path.to_str().unwrap().to_string());
+        return Ok(desktop_icon_path
+            .to_str()
+            .map(|s| s.to_string())
+            .ok_or_else(|| {
+                io::Error::new(
+                    ErrorKind::Other,
+                    format!("failed to convert path to string: {:?}.", desktop_icon_path),
+                )
+            })?);
     } else {
         let manifest_path = package_folder.join("AppxManifest.xml");
-        let manifest_content = fs::read_to_string(&manifest_path)?;
+        if manifest_path.exists() {
+            let manifest_content = fs::read_to_string(&manifest_path).map_err(|_| {
+                io::Error::new(ErrorKind::Other, "could not to read the AppxManifest.xml.")
+            })?;
 
-        let icon_path = extract_icon_path(&manifest_content)?;
-        let icon_full_path = package_folder.join(icon_path);
-
-        return Ok(icon_full_path.to_str().unwrap().to_string());
+            let icon_path = extract_icon_path(&manifest_content)?;
+            let icon_full_path = package_folder.join(icon_path);
+            if icon_full_path.exists() {
+                return Ok(icon_full_path
+                    .to_str()
+                    .map(|s| s.to_string())
+                    .ok_or_else(|| {
+                        io::Error::new(
+                            ErrorKind::Other,
+                            format!("failed to convert path to string: {:?}.", icon_full_path),
+                        )
+                    })?);
+            } else {
+                return Err(Box::new(io::Error::new(
+                    ErrorKind::NotFound,
+                    format!(
+                        "icon path provided by the manifest does not exist: {:?}.",
+                        icon_full_path
+                    ),
+                )));
+            }
+        } else {
+            return Err(Box::new(io::Error::new(
+                ErrorKind::NotFound,
+                format!("AppxManifest.xml does not exist: {:?}.", manifest_path),
+            )));
+        }
     }
 }
 
@@ -49,8 +108,8 @@ fn extract_icon_path(manifest_content: &str) -> Result<String, Box<dyn Error>> {
         }
     }
 
-    Err(Box::new(std::io::Error::new(
-        std::io::ErrorKind::NotFound,
-        "Icon path not found in manifest.",
+    Err(Box::new(io::Error::new(
+        ErrorKind::NotFound,
+        "icon path not found in manifest.",
     )))
 }
